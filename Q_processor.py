9f6ac5fc8e38315964818fcdd47621c0aa42d64f
Ubuntu <ubuntu@ip-172-31-64-241.ec2.internal>
2025-04-26 18:06:11 +0000

    no_pupils

--- Q_processor.py
+++ Q_processor.py
@@ -115,7 +115,7 @@ class ImagePreprocessingQEnv:
         # print("Rewards saved to 'rewards.txt' and plot saved as 'rewards_plot.png'.")
 
 if __name__ == "__main__":
-    num_bins = 5
+    num_bins = 10
     num_actions = num_bins * num_bins
     q_table = np.zeros((num_bins, num_bins, num_actions))
 
@@ -127,7 +127,7 @@ if __name__ == "__main__":
     num_episodes = 10000
 
     model_path = "models/YOLO_eye_detector.pt"
-    image_folder = "images/test"
+    image_folder = "images/no_pupil"
     detector = ObjectDetectorCNN(model_path)
     env = ImagePreprocessingQEnv(detector, image_folder, render=False, num_bins=num_bins)
 
